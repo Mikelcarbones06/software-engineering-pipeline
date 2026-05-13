f26e7887ed6952f71d7140fa69708409d9e5bc56
agusciaponi <agusciaponi@gmail.com>
2026-05-13 15:23:23 +0200

    division implementation

--- test_calculator.py
+++ test_calculator.py
@@ -1,4 +1,5 @@
-from calculator import add, sub
+from calculator import add, sub, multiply, divide
+
 def test_add():
     assert add(2, 3) == 5
 def test_sub():
@@ -6,3 +7,6 @@ def test_sub():
 def test_multiply():
     assert multiply(3, 4) == 12
     assert multiply(0, 5) == 0
+def test_divide():
+    assert divide(10, 4) == 2.5
+    assert divide(20, 2) == 10
